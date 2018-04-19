a2398c7ddbfead174aa06f6db5112281204a078d
Emmanuel KWENE <njume48@gmail.com>
2018-04-19 09:15:08 +0100

    Split code in shortcodes

--- functions.php
+++ functions.php
@@ -3,92 +3,112 @@
 /**
  * Locate template to load
  */
-if( !function_exists('wft_locate_template') )
-{
-	/**
-	 * @param string $template_name Template to load.
-	 * @param string $template_path Path to templates.
-	 * @param string $default_path  Default path to template files.
-	 * @return string $template     Path to the template file.
-	 */
-	function wft_locate_template($template_name, $template_path = '', $default_path = '')
-	{
-		// Set default plugin templates path
-		if( !$default_path ) :
-			$default_path = plugin_dir_path(__FILE__) . 'includes/templates/'; // Path to the templates folder.
-		endif;
+if (!function_exists('wft_locate_template')) {
+    /**
+     * @param string $template_name Template to load.
+     * @param string $template_path Path to templates.
+     * @param string $default_path  Default path to template files.
+     * @return string $template     Path to the template file.
+     */
+    function wft_locate_template($template_name, $template_path = '', $default_path = '')
+    {
+        // Set default plugin templates path
+        if (!$default_path) :
+            $default_path = plugin_dir_path(__FILE__) . 'includes/templates/'; // Path to the templates folder.
+        endif;
 
-		// Search template file in theme folder
-		$template = locate_template(array(
-			$template_path . $template_name,
-			$template_name
-		));
+        // Search template file in theme folder
+        $template = locate_template([
+            $template_path . $template_name,
+            $template_name
+        ]);
 
-		// Get plugins template file if not found into theme.
-		if( !$template ) :
-			$template = $default_path . $template_name;
-		endif;
+        // Get plugins template file if not found into theme.
+        if (!$template) :
+            $template = $default_path . $template_name;
+        endif;
 
-		return apply_filters('wft_locate_template', $template, $template_name, $template_path, $default_path);
-	}
+        return apply_filters('wft_locate_template', $template, $template_name, $template_path, $default_path);
+    }
 }
 
 
 /**
  * Include the template
  */
-if( !function_exists('wft_get_template') )
-{
-	/**
-	 * @param $template_name
-	 * @param array $args
-	 * @param string $template_path
-	 * @param string $default_path
-	 */
-	function wft_get_template($template_name, $args = array(), $template_path = '', $default_path = '')
-	{
-		if( is_array($args) && isset($args) ) :
-			extract($args);
-		endif;
+if (!function_exists('wft_get_template')) {
+    /**
+     * @param $template_name
+     * @param array $args
+     * @param string $template_path
+     * @param string $default_path
+     */
+    function wft_get_template($template_name, $args = [], $template_path = '', $default_path = '')
+    {
+        if (is_array($args) && isset($args)) :
+            extract($args);
+        endif;
 
-		$template_file = wft_locate_template($template_name, $template_path, $default_path);
+        $template_file = wft_locate_template($template_name, $template_path, $default_path);
 
-		if( !file_exists($template_file) ):
-			_doing_it_wrong(__FUNCTION__, sprintf('<code>%s</code> does not exist.', $template_file), '1.0.0');
-		endif;
+        if (!file_exists($template_file)) :
+            _doing_it_wrong(__FUNCTION__, sprintf('<code>%s</code> does not exist.', $template_file), '1.0.0');
+        endif;
 
-		include $template_file;
-	}
+        include $template_file;
+    }
 }
 
 /**
  * Enqueue style & script
  */
-function wft_enqueue_script() {
+function wft_enqueue_script()
+{
 
-	// Enqueue stylesheet
-	wp_enqueue_style(
-		'fornite-tracker',
-		plugin_dir_url(__FILE__) . 'assets/css/main.css',
-		false
-	);
+    // Enqueue stylesheet
+    wp_enqueue_style(
+        'fornite-tracker',
+        plugin_dir_url(__FILE__) . 'assets/css/main.css',
+        false
+    );
 
-	// Enqueue scripts
-	wp_enqueue_script(
-		'fornite-tracker-js',
-		plugin_dir_url(__FILE__) . 'assets/js/script.js',
-		array('jquery'),
-		'1.0',
-		true
-	);
+    // Enqueue scripts
+    wp_enqueue_script(
+        'fornite-tracker-js',
+        plugin_dir_url(__FILE__) . 'assets/js/script.js',
+        ['jquery'],
+        '1.0',
+        true
+    );
 }
-add_action( 'wp_enqueue_scripts', 'wft_enqueue_script' );
+add_action('wp_enqueue_scripts', 'wft_enqueue_script');
 
 
 /**
- * Shortcode search term
+ * Shortcode displaying the form
  */
-function wft_fornite_search_shortcode() {
-	return wft_get_template('fornite-search.php');
+add_shortcode('fornite_tracker_form', 'wft_fornite_search_shortcode');
+function wft_fornite_search_shortcode()
+{
+    $fornite_search_form_code = wft_get_template('fornite-search.php');
+    return apply_filters('fornite_tracker_form', $fornite_search_form_code);
 }
-add_shortcode('fornite_tracker', 'wft_fornite_search_shortcode');
+
+
+/**
+ * Shortcode displaying the result
+ */
+add_shortcode('fornite_tracker_result', 'wft_fornite_result_shortcode');
+function wft_fornite_result_shortcode()
+{
+    $fornite_tracker_result_code = '';
+
+    if (!empty($_REQUEST['wp_fornite_platform']) && !empty($_REQUEST['wp_fornite_uname'])) {
+        $platform = strip_tags($_REQUEST['wp_fornite_platform']);
+        $username = strip_tags($_REQUEST['wp_fornite_uname']);
+
+        $fornite_tracker_result_code = wft_get_template('fornite-tracker.php');
+    }
+
+    return apply_filters('fornite_tracker_result', $fornite_tracker_result_code);
+}
--- includes/templates/fornite-search.php
+++ includes/templates/fornite-search.php
@@ -1,30 +1,24 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Don't allow direct access ?>
+if (! defined('ABSPATH')) {
+    exit;
+} // Don't allow direct access?>
 
 <div class='wp-fornite-search-from'>
 
-	<form method='post'>
-		<label><input type="radio" name="wp_fornite_platform" value="pc"> PC</label>
-		<label><input type="radio" name="wp_fornite_platform" value="xbl"> Xbox one </label>
-		<label><input type="radio" name="wp_fornite_platform" value="psn"> Play Station</label>
-		<br>
-		<label>
-			<strong>
-				<?php _e( 'Stats Fornite', 'wft' ); ?>
-			</strong>
-			<input type='text' name='wp_fornite_uname' id='wp-fornite-uname-input'>
-		</label>
+    <form method='post'>
+        <label><input type="radio" name="wp_fornite_platform" value="pc"> PC</label>
+        <label><input type="radio" name="wp_fornite_platform" value="xbl"> Xbox one </label>
+        <label><input type="radio" name="wp_fornite_platform" value="psn"> Play Station</label>
+        <br>
+        <label>
+            <strong>
+                <?php _e('Stats Fornite', 'wft'); ?>
+            </strong>
+            <input type='text' name='wp_fornite_uname' id='wp-fornite-uname-input'>
+        </label>
 
-		<input type='submit' value="<?php _e('Afficher', 'wft'); ?>">
-	</form>
+        <input type='submit' value="<?php _e('Afficher', 'wft'); ?>">
+    </form>
 
 </div>
-
-<?php
-if( !empty($_REQUEST['wp_fornite_platform']) && !empty($_REQUEST['wp_fornite_uname']) ){
-
-	$platform = strip_tags($_REQUEST['wp_fornite_platform']);
-	$username = strip_tags($_REQUEST['wp_fornite_uname']);
-	include 'fornite-tracker.php';
-}
